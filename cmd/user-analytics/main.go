1b2daec18a8f86f582c304674fbb4524751c7fca
tushkanchek228 <maximkibardin2005@gmail.com>
2025-09-26 19:16:25 +0300

    create main file + TODO list

---
+++ cmd/user-analytics/main.go
@@ -0,0 +1,33 @@
+package main
+
+
+
+
+const (
+	envLocal = "local"
+	envDev = "dev"
+	envProd = "prod"
+)
+
+
+func main() {
+	
+	//TODO: init config
+
+
+	//TODO: init logger
+
+
+	//TODO: init storage
+
+
+	//TODO: init kafka
+
+
+	//TODO: init chi router
+
+
+	//TODO: run server
+
+
+}
