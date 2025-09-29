a60751b79ed9d52f46c58bb9ade594a5db1773cd
tushkanchek228 <maximkibardin2005@gmail.com>
2025-09-29 12:25:31 +0300

    pretty log init

--- cmd/user-analytics/main.go
+++ cmd/user-analytics/main.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"log/slog"
+	"os"
+	"user-analytics/internal/config"
+	"user-analytics/internal/lib/logger/sl"
+	"user-analytics/internal/lib/logger/slogpretty"
+	"user-analytics/internal/storage/postgres"
 
+	"github.com/joho/godotenv"
+)
 
+// go run ./cmd/user-analytics/main.go
 
 const (
 	envLocal = "local"
@@ -12,13 +22,20 @@ const (
 
 func main() {
 	
-	//TODO: init config
+	initConfig()
+	cfg := config.MustLoad()
+	
+
+	log := setupLogger(cfg.Env)
+
+	log.Info(
+		"starting user-analytics",
+		slog.String("env", cfg.Env),
+		slog.String("version", "666"),
+	)
+	log.Debug("debug messages enabled")
 
 
-	//TODO: init logger
-
-
-	//TODO: init storage
 
 
 	//TODO: init kafka
@@ -31,3 +48,39 @@ func main() {
 
 
 }
+func initConfig(){
+	if err := godotenv.Load();err!=nil{
+		os.Exit(1)
+	}
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+
+	handler := opts.NewPrettyHandler(os.Stdout)
+
+	return slog.New(handler)
+}
+
